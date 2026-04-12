16cd9653f109ce021428c7b7935f7ee4e2ceb1d7
Destroyerofdarkness <dawidadogier@gmail.com>
2026-04-12 22:28:28 +0200

    Made authentication route for verifing JWT and sending back user

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,12 +1,13 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const { handler_user_errors } = require("../handlers/modelErrorHandler");
+
 const signJWT = (id) => {
   return jwt.sign({ id }, process.env.secret, {
     expiresIn: 24 * 60 * 60,
   });
 };
-
+//Sign in & Sign Up
 const sign_in_user = async (req, res) => {
   const { BODY } = req.body;
   try {
@@ -48,7 +49,40 @@ const sign_up_user = async (req, res) => {
   }
 };
 
+//JWT verification
+
+const sendBackUserAndVerify= async (req, res) => {
+  const token = req.params.token;
+  try {
+    jwt.verify(token, process.env.secret, async (err, decodedToken) => {
+      if (err) {
+        console.log("Err on JWT verification: ", err);
+        res.status(400).json({
+          success: false,
+          message: `Failed to verify JWT cause: ${err.message}`,
+        });
+      } else {
+        console.log(decodedToken);
+        const user = await User.findById(decodedToken.id);
+        console.log(user);
+        res.status(201).json({
+          success: true,
+          user: user,
+          message: "JWT token verified and user acquiered",
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({err,success:false, message: "Failed to verify JWT cause: Internal Server Error" });
+  }
+};
+
+
 module.exports = {
   sign_in_user,
   sign_up_user,
+  sendBackUserAndVerify
 };
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -9,4 +9,6 @@ router.post("/signIn", authorization, controller.sign_in_user);
 
 router.post("/signUp", authorization, controller.sign_up_user);
 
+router.get("/verifyJWT/:token",authorization, controller.sendBackUserAndVerify);
+
 module.exports = router
